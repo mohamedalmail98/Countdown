684ecf08385fcfce363bce2c37555a03a5743ecb
mohamedalmail98 <malmail@aus.edu>
2025-07-17 09:50:04 +0400

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -22,7 +22,7 @@ st.markdown("""
         position: fixed;
         top: 0; left: 0;
         height: 100vh; width: 100vw;
-        background-image: url('https://images.unsplash.com/photo-1477201389074-1863f668fac1?ixlib=rb-4.0.3&auto=format&fit=crop&w=1950&q=80');
+        background-image: url('https://images.unsplash.com/photo-1475274047050-1d0c0975c63e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3BhY2UlMjBiYWNrZ3JvdW5kfGVufDB8fDB8fHww');
         background-size: cover;
         background-position: center;
         background-attachment: fixed;
